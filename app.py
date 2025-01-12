42c61e5cb3b992336547adcb6086f9aef47ecabb
Waddhanabot Yi <104647438+WBotCS@users.noreply.github.com>
2025-01-12 03:02:32 -0800

    Update 2 app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def select2_data():
 
     return jsonify({'notes': notes_data, 'families': families_data})
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port, debug=True)
 
